a105fb0f25bc3d80dc9bac0f0a3f01f06d486235
Justin Pauga <paugajt@gmail.com>
2017-09-16 13:37:28 -0600

    Initial Commit

---
+++ src/main/java/com/rebelscrum/MSUBanner/Application/MsuBannerApplication.java
@@ -0,0 +1,12 @@
+package com.rebelscrum.MSUBanner.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsuBannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsuBannerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rebelscrum/MSUBanner/Application/MsuBannerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rebelscrum.MSUBanner.Application;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsuBannerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
