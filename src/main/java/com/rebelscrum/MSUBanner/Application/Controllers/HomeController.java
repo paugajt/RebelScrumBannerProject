fd909f640f0c2c103078f97c6bbe27a689da288c
Justin Pauga <paugajt@gmail.com>
2017-09-19 10:00:38 -0600

    Cleaned up code for readability

--- src/main/java/com/rebelscrum/MSUBanner/Application/Controllers/HomeController.java
+++ src/main/java/com/rebelscrum/MSUBanner/Application/Controllers/HomeController.java
@@ -11,4 +11,6 @@ public class HomeController {
 
     @RequestMapping("/")
     public String home() {return "index";}
+
+
 }
--- src/main/java/com/rebelscrum/MSUBanner/Application/MsuBannerApplication.java
+++ src/main/java/com/rebelscrum/MSUBanner/Application/MsuBannerApplication.java
@@ -4,6 +4,7 @@ import com.rebelscrum.MSUBanner.Application.Entity.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MsuBannerApplication {
 
