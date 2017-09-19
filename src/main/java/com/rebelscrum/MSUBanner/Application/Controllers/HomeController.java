9598de91c529a2ef1d89321b8ae88a2eaeff99fd
Justin Pauga <paugajt@gmail.com>
2017-09-19 12:34:38 -0600

    Added delete method and fields to show email on the webpage

--- src/main/java/com/rebelscrum/MSUBanner/Application/Controllers/HomeController.java
+++ src/main/java/com/rebelscrum/MSUBanner/Application/Controllers/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @RequestMapping("/")
-    public String home() {return "index";}
+    public String home() {return "Index";}
 
 
 }
