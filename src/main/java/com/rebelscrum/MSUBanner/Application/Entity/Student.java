cbeb5777860336550ae15699e613396d9150d51b
pperez10 <pperez10@msudenver.edu>
2017-09-28 10:16:33 -0600

    bug fixes implemented

--- src/main/java/com/rebelscrum/MSUBanner/Application/Entity/Student.java
+++ src/main/java/com/rebelscrum/MSUBanner/Application/Entity/Student.java
@@ -6,20 +6,23 @@ import javax.persistence.*;
 @Entity
 public class Student extends User {
     @Id
-
     @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
     private String major;
     private Integer creditsEarned;
     private Integer creditLevel;
 
-    public String major() {
+    public String getMajor() {
         return major;
     }
     public void setMajor(String major) {
         this.major = major;
     }
 
-    public Integer creditsEarned(){
+    public Integer getCreditsEarned(){
         return creditsEarned;
     }
     public void setCreditsEarned(Integer creditsEarned) {
@@ -32,6 +35,38 @@ public class Student extends User {
     public void setCreditLevel(Integer creditLevel) {
         this.creditLevel = creditLevel;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
 
 
 
