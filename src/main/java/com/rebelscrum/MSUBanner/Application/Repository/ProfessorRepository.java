a85a4873fff9591fccc3bdeed0c080e8176fbed4
MaxMorrison6 <maxdmorrison6@gmail.com>
2017-09-28 09:43:39 -0600

    Committing professor repository

---
+++ src/main/java/com/rebelscrum/MSUBanner/Application/Repository/ProfessorRepository.java
@@ -0,0 +1,10 @@
+package com.rebelscrum.MSUBanner.Application.Repository;
+
+import com.rebelscrum.MSUBanner.Application.Entity.Professor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProfessorRepository extends CrudRepository<Professor, Integer> {
+
+
+}
+
