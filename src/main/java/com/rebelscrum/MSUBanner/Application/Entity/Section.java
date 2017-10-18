7b4a7d842c05c9b3bf5d77defbb89048b29ea873
hassankhokhar <hassan@fieldsquared.com>
2017-10-18 17:02:40 -0600

    Update Section.java

--- src/main/java/com/rebelscrum/MSUBanner/Application/Entity/Section.java
+++ src/main/java/com/rebelscrum/MSUBanner/Application/Entity/Section.java
@@ -7,6 +7,14 @@ public class Section {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
+    
+    // These are suppose to be real Course and Professor types such as 'private Professor professor';
+    // Once you add the the real types you need to add OneToOne annotation here which says 'each section has 
+    // one professor and one course'
+    //  @OneToOne
+    // The annotation will need to know what is the foriegin key that you are trying to join with this so you
+    //  @JoinColumn(name="") <-- the name is the column name of what you have defined as the foregin key in the professor/course entity
+    
     private String course;
     private String professor;
 
