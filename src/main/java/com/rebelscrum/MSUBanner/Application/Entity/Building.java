df57df22ebf2ed1d8b6e811133cc8a0f9c00b321
Max <maxdmorrison6@gmail.com>
2017-10-26 10:57:37 -0600

    Edited Building to remove Room and roomCount, need to edit Room & Departments to array list from array Removed roomCount from loader Added Building to Header, getting white page error on buildings page

--- src/main/java/com/rebelscrum/MSUBanner/Application/Entity/Building.java
+++ src/main/java/com/rebelscrum/MSUBanner/Application/Entity/Building.java
@@ -1,9 +1,6 @@
 package com.rebelscrum.MSUBanner.Application.Entity;
 
-import org.hibernate.dialect.Ingres9Dialect;
-
 import javax.persistence.*;
-import javax.persistence.criteria.CriteriaBuilder;
 
 @Entity
 public class Building {
@@ -12,7 +9,6 @@ public class Building {
     private Integer id;
     private String buildingName;
     private String address;
-    private Integer roomCount;
     private Room[] roomList;
     private String[] departments;
 
@@ -25,31 +21,10 @@ public class Building {
     public  void setAddress(String address) {this.address = address;}
     public String getAddress() {return address;}
 
-    public void setRoomCount(Integer roomCount) {this.roomCount = roomCount;}
-    public Integer getRoomCount() {return  roomCount;}
-
     public void setRoomList(Room[] roomList){this.roomList = roomList;}
     public Room[] getRoomList() {return roomList;}
 
-    public  void setDepartments(String[] departments, int index) {this.departments = departments;}
+    public  void setDepartments(String[] departments) {this.departments = departments;}
     public String[] getDepartments() {return departments;}
-
-    class Room{
-        private Integer roomNum;
-        private Integer seats;
-
-        Room(Integer roomNum, Integer seats){
-            this.roomNum = roomNum;
-            this.seats = seats;
-        }
-
-        public  void setRoomNum(Integer roomNum) {this.roomNum = roomNum;}
-        public Integer getRoomNum() {return roomNum;}
-
-        public  void setSeats(Integer seats) {this.seats = seats;}
-        public Integer getSeats() {return seats;}
-
-
-    }
 
 }
