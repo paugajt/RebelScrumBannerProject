a9a8c21fd358b086b38dc28923c4d412b695aeee
Peter <pperez10@msudenver.edu>
2017-11-02 10:20:45 -0600

    minor changes to schedule functions

--- src/main/java/com/rebelscrum/MSUBanner/Application/Entity/Schedule.java
+++ src/main/java/com/rebelscrum/MSUBanner/Application/Entity/Schedule.java
@@ -1,59 +1,43 @@
 package com.rebelscrum.MSUBanner.Application.Entity;
 import javax.persistence.*;
 import java.security.PrivateKey;
+import java.util.ArrayList;
 
 @Entity
 public class Schedule {
-    private Object Semester;
-    private Object Building;
-    private Object Course;
+    private Semester semester;
+    private Building building;
+    private ArrayList<Course> courseList= new ArrayList<>();
     private String days;
 
     final String MW = "mw";
     final String TR = "tr";
     final int time = 2;
 
-    public Object getSemester() {
-        return Semester;
+    public Semester getSemester() {return semester;}
+
+    public void setSemester(Semester semester) {this.semester = semester;}
+
+    public Building getBuilding() {return building;}
+
+    public void setBuilding(Building building) {this.building = building;}
+
+    public String getDays() {return days;}
+
+    public void setDays(String days) {this.days = days;}
+
+    public String getMW() {return MW;}
+
+    public String getTR() {return TR; }
+
+    public int getTime() {return time; }
+
+    public void addCourse(Course course){
+        this.courseList.add(course);
     }
 
-    public void setSemester(Object semester) {
-        Semester = semester;
+    public ArrayList<Course> getCourseList(){
+        return courseList;
     }
 
-    public Object getBuilding() {
-        return Building;
-    }
-
-    public void setBuilding(Object building) {
-        Building = building;
-    }
-
-    public Object getCourse() {
-        return Course;
-    }
-
-    public void setCourse(Object course) {
-        Course = course;
-    }
-
-    public String getDays() {
-        return days;
-    }
-
-    public void setDays(String days) {
-        this.days = days;
-    }
-
-    public String getMW() {
-        return MW;
-    }
-
-    public String getTR() {
-        return TR;
-    }
-
-    public int getTime() {
-        return time;
-    }
-}
+ }
