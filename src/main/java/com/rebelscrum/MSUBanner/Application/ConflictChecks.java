c2d0c11e089d4c32e32f4d1543ca7b331bdcb834
Peter <pperez10@msudenver.edu>
2017-11-30 14:37:31 -0700

    fixed SectionTest

--- src/main/java/com/rebelscrum/MSUBanner/Application/ConflictChecks.java
+++ src/main/java/com/rebelscrum/MSUBanner/Application/ConflictChecks.java
@@ -1,36 +1,25 @@
 package com.rebelscrum.MSUBanner.Application;
-
 import com.rebelscrum.MSUBanner.Application.Entity.Schedule;
 import com.rebelscrum.MSUBanner.Application.Entity.Section;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
 public class ConflictChecks {
 
-
-
     //Checks time and day conflicts for course to be added against courses already in course list
-    public boolean conflicts(Section sectionToAdd){
+    public boolean conflicts(Section courseToAdd){
         boolean checkDays = true;
         boolean checkTimes = true;
-        int j = Schedule.sectionList.size();
-        Schedule[] sectionArray=Schedule.sectionList.toArray(new Schedule[j]);
-
+        Section[] sectionList = new Section[Schedule.sectionList.size()];
+        Schedule.sectionList.toArray(sectionList);
         for (int i = 0; i < Schedule.sectionList.size(); i++){
-            if (sectionToAdd.getDays() != sectionArray[i].getDays()){
+            if (courseToAdd.getDays() != sectionList[i].getDays()){
                 checkDays = false;
             }
-
-            if (!(sectionToAdd.getTimeStart() > sectionArray[i].getTimeStart) &&
-                    !(sectionToAdd.getTimeEnd() < sectionArray[i].getTimeEnd())){
+            if (!(courseToAdd.getTimeStart() > sectionList[i].getTimeStart()) &&
+                    !(courseToAdd.getTimeEnd() < sectionList[i].getTimeEnd())){
                 checkTimes = false;
             }
-
             if (checkDays && checkTimes){
                 return true;
             }
-
         }
         return false;
     }
