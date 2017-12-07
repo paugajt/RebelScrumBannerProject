4d76709639cddd14ea8ee843b9707d6deff9628b
Max <maxdmorrison6@gmail.com>
2017-12-07 09:23:06 -0700

    Building Entity Test

---
+++ src/test/java/com/rebelscrum/MSUBanner/Application/Entity/BuildingTest.java
@@ -0,0 +1,57 @@
+package com.rebelscrum.MSUBanner.Application.Entity;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BuildingTest {
+
+    @Test
+    public void setId() throws Exception{
+        Building tester = new Building();
+        assertNull("This test should pass when there is no Building ID", tester.getId());
+        tester.setId(1234);
+        assertNotNull("The Building ID exists", tester.getId());
+    }
+
+    @Test
+    public void getId() throws  Exception{
+        Building tester = new Building();
+        assertNull("This test should pass when there is no Building ID", tester.getId());
+        tester.setId(1234);
+        assertNotNull("The Building ID exists", tester.getId());
+    }
+
+    @Test
+    public void setBuildingName() throws Exception{
+        Building tester = new Building();
+        assertNull("This test should pass when there is not Building Name", tester.getBuildingName());
+        tester.setBuildingName("Test Science");
+        assertNotNull("The Building has a name", tester.getBuildingName());
+    }
+
+    @Test
+    public void getBuildingName() throws Exception{
+        Building tester = new Building();
+        assertNull("This test should pass when there is not Building Name", tester.getBuildingName());
+        tester.setBuildingName("Test Science");
+        assertNotNull("The Building has a name", tester.getBuildingName());
+    }
+
+    @Test
+    public void setAddress() throws Exception{
+        Building tester = new Building();
+        assertNull("This test should pass when there is not Building address", tester.getAddress());
+        tester.setAddress("1234 Auraria");
+        assertNotNull("The Building has an address", tester.getAddress());
+
+    }
+
+    @Test
+    public void getAddress() throws Exception{
+        Building tester = new Building();
+        assertNull("This test should pass when there is not Building address", tester.getAddress());
+        tester.setAddress("1234 Auraria");
+        assertNotNull("The Building has an address", tester.getAddress());
+    }
+}
