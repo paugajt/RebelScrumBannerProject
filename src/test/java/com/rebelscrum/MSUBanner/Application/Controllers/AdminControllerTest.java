59597acd5d8dac90327dd524b128c011e74ccfac
Sang Nguyen <snguye39@msudenver.edu>
2017-12-07 09:46:20 -0700

    admin fix

--- src/test/java/com/rebelscrum/MSUBanner/Application/Controllers/AdminControllerTest.java
+++ src/test/java/com/rebelscrum/MSUBanner/Application/Controllers/AdminControllerTest.java
@@ -7,7 +7,6 @@ import com.rebelscrum.MSUBanner.Application.Services.AdminService;
 import com.rebelscrum.MSUBanner.Application.Services.AdminServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
