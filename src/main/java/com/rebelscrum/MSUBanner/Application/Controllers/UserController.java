e0dea19d609d5c817ce6b374ec8c5e1a22f69ebb
klphillips <klphillips1206@gmail.com>
2017-12-07 08:21:30 -0700

    Committing lots of stuff for authentication

--- src/main/java/com/rebelscrum/MSUBanner/Application/Controllers/UserController.java
+++ src/main/java/com/rebelscrum/MSUBanner/Application/Controllers/UserController.java
@@ -2,19 +2,27 @@ package com.rebelscrum.MSUBanner.Application.Controllers;
 
 import com.rebelscrum.MSUBanner.Application.Entity.User;
 import com.rebelscrum.MSUBanner.Application.Services.UserService;
+import com.rebelscrum.MSUBanner.Application.Services.SecurityService;
+import com.rebelscrum.MSUBanner.Application.Validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
-
 @Controller
 public class UserController {
-
+    @Autowired
     private UserService userService;
+
+    @Autowired
+    private SecurityService securityService;
+
+    @Autowired
+    private UserValidator userValidator;
 
     @Autowired
     public void setUserService(UserService userService){
@@ -37,6 +45,28 @@ public class UserController {
     public String edit(@PathVariable Integer id, Model model){
         model.addAttribute("user", userService.getUserById(id));
         return "userform";
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public String registration(Model model) {
+        model.addAttribute("userForm", new User());
+
+        return "registration";
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult, Model model) {
+        userValidator.validate(userForm, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+
+        userService.save(userForm);
+
+        securityService.autologin(userForm.getUsername(), userForm.getPasswordConfirm());
+
+        return "redirect:/welcome";
     }
 
     @RequestMapping("user/new")
