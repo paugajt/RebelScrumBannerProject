7d197bc125bd6586726aef1b0eb37f6787f2dcf3
klphillips <klphillips1206@gmail.com>
2017-12-07 08:33:31 -0700

    Committing lots of stuff for authentication

---
+++ src/main/java/com/rebelscrum/MSUBanner/Application/Services/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.rebelscrum.MSUBanner.Application.Services;
+
+import com.rebelscrum.MSUBanner.Application.Entity.User;
+import com.rebelscrum.MSUBanner.Application.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), null);
+    }
+
+}
